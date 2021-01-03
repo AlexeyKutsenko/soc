35fb5b997861b5050a991c2669ea3c1c3278cedd
AlexeyKutsenko <kutsenko.new@gmail.com>
2021-01-03 22:39:34 +0200

    Clearbit integration has been added

--- soc/user/urls.py
+++ soc/user/urls.py
@@ -1,8 +1,10 @@
-from django.urls import include, re_path
+from django.urls import re_path
 from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token
 
+from user.views import RegisterView
+
 urlpatterns = [
-    re_path(r'^registration/', include('rest_auth.registration.urls')),
+    re_path(r'^registration/$', RegisterView.as_view(), name='rest_register'),
     re_path(r'^token-auth/', obtain_jwt_token),
     re_path(r'^token-verify/', verify_jwt_token)
 ]
---
+++ soc/user/views.py
@@ -0,0 +1,24 @@
+import clearbit
+from rest_auth.registration.views import RegisterView as BaseRegisterView
+from rest_framework import status
+from rest_framework.response import Response
+
+
+class RegisterView(BaseRegisterView):
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        clearbit_enrich = clearbit.Enrichment.find(email=request.data.get('email'), stream=True)
+        try:
+            user.first_name = clearbit_enrich['person'].get('name').get('givenName')
+            user.last_name = clearbit_enrich['person'].get('name').get('familyName')
+            user.save()
+        except TypeError:
+            pass
+
+        return Response(self.get_response_data(user),
+                        status=status.HTTP_201_CREATED,
+                        headers=headers)
